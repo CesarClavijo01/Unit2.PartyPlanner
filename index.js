434568b5e8f596ac491a26af65232a782ab080b9
CesarClavijo01 <cesar@Cesar>
2024-03-22 22:31:52 -0500

    finished functional workshop

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ async function getEvents(){
 
     }
     return json.data;
+    
 }
 
 //create events
 async function createEvent(event){
-    const response = await fetch('${baseURL}/events', {
+    const response = await fetch(`${baseURL}/events`, {
         method: 'post',
         headers: 
         {"Content-Type": "application/json"},
@@ -28,26 +29,36 @@ async function createEvent(event){
     if(!json.success){
         throw new Error(json.error);
     }
-        return json.data
+        return json.data;
 
 }
 
 
 //display events
 function addEventsToScreen(event){
+    const deleteBtn = document.createElement("button");
     const eventDiv = document.createElement("div");
     eventDiv.classList.add("eventDiv");
     const eventName = document.createElement("h2");
+    eventName.classList.add("name")
     const eventDate = document.createElement("h3");
     const eventDescription = document.createElement("a");
     const eventLocaion = document.createElement("a");
-    const space = document.createElement("br")
+    const space = document.createElement("br");
+    const deleteText = document.createTextNode("X");
+    deleteBtn.classList.add("deleteBtn");
+    const idContainer = document.createElement("h2");
+    idContainer.classList.add("eventId");
 
+    idContainer.append("#" + event.id);
+    deleteBtn.append(deleteText);
     eventName.append(event.name);
     eventDate.append(event.date);
     eventDescription.append(event.description);
     eventLocaion.append(event.location);
 
+    eventDiv.append(deleteBtn);
+    eventDiv.append(idContainer);
     eventDiv.append(eventName);
     eventDiv.append(eventDate);
     eventDiv.append(eventDescription);
@@ -55,7 +66,19 @@ function addEventsToScreen(event){
     eventDiv.append(eventLocaion);
 
     eventContainer.appendChild(eventDiv);
-}
+
+    deleteBtn.addEventListener("click", async (event) => {
+        //get the id of the event we want to delete
+        const selectedEvent = event.target.closest(".eventDiv");
+        const id = selectedEvent.querySelector(".eventId").innerText.slice(1);
+    
+        //delete the event from the DB
+        deleteEvents(id)
+
+        //delete the event from the screen
+        selectedEvent.remove()
+    })
+};
 
 document.addEventListener('DOMContentLoaded', async () => {
     const events = await getEvents();
@@ -63,6 +86,49 @@ document.addEventListener('DOMContentLoaded', async () => {
     events.forEach(event => {
         addEventsToScreen(event)
     })
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById("name");
+        const date = document.getElementById("date");
+        const description = document.getElementById("description");
+        const location = document.getElementById("location");
+
+        const newEvent = {
+            name: name.value,
+            date: date.value,
+            description: description.value,
+            location: location.value
+        };
+        
+        try{
+            const postedEvent = await createEvent(newEvent);
+            console.log(postedEvent);
+
+            addEventsToScreen(postedEvent);
+        }
+        catch{
+            console.error(err);
+        }
+    })
 });
 
+
+
 //delte events
+async function deleteEvents(id){
+    const response = await fetch(`${baseURL}/events/${id}`, {
+        method: 'delete'
+    });
+
+
+    if(response.status === 204){
+
+        return true;
+
+    }
+
+    throw new Error(`Sorry we can't delete the event with Id ${id}`);
+} 
+
